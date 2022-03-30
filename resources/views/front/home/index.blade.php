6703a44023655bde8fc3803944a029a96d617ecb
vicdenys <vicdenys@hotmail.com>
2022-03-30 12:57:52 +0200

    wip

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -9,20 +9,31 @@
 <section class="grid grid-cols-5 container mx-auto">
     <div class="col-span-5 inline-grid items-start py-4 grid-cols-5 border-b border-black">
         <div class=" flex items-center gap-4">
-            <img class="h-6" src="/images/logo.svg" alt="logo spatie">
-            <p>presents</p>
+            <a href="https://spatie.be">
+                <img class="h-6" src="/images/logo.svg" alt="logo spatie">
+            </a>
+            
+            <p class="text-sm">presents</p>
         </div>
-        <h1 class="col-span-3 text-7xl font-normal uppercase">Laravel</h1>
+        <h1 class="col-span-3 text-7xl font-normal uppercase leading-[0.74]">Laravel</h1>
     </div>
     <div class="col-span-5 inline-grid items-start py-4 grid-cols-5 border-b border-black">
-        <h1 class="col-span-3 col-start-2 text-7xl font-bold flex items-end gap-4 uppercase">
+        <h1 class="col-span-3 col-start-2 text-7xl font-bold flex items-end gap-4 uppercase leading-[0.74]">
             Comments
-            <img src="/images/github.svg" alt="logo github">
+            <a href="https://github.com/spatie/laravel-comments"><img src="/images/github.svg" alt="logo github"></a>
+            
         </h1>
 
     </div>
     <div class="col-span-5 inline-grid items-start py-4 pb-32 grid-cols-5">
-        <p class="col-start-2 col-span-2">A package for laravel. Get a comments section up an running in no time</p>
+            <div class="col-start-2 col-span-2">
+                
+                <p>
+                    <div class="w-3 h-3 inline-block bg-yellow"></div>
+                    A package for laravel. Get a comments section up an running in no time
+                </p>
+                
+            </div>
     </div>
     
 </section>
