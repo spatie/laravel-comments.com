51d0f195ac600c53cd1959d3ba9805f8557ff64e
vicdenys <vicdenys@hotmail.com>
2022-03-30 16:04:31 +0200

    wip

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -72,25 +72,25 @@
 </section>
 <section class="container mx-auto mt-4 pt-20  sm:border-t border-black  px-8 sm:px-0">
     <h1 class="uppercase text-7xl mb-8 py-4">Why<span class="font-bold"> This Package</span></h1>
-    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
-        <div class="col-span-1 border  border-black  border-b-0 md:border-b md:border-r-0 p-4 hover:bg-gray-100 transition">
+    <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5">
+        <div class="col-span-1 border  border-black  border-b-0 sm:border-b sm:border-r-0 p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-yellow"></div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el
             </p>
         </div>
-        <div class="col-span-1 hidden md:block border border-t-0  border-black p-4">
+        <div class="col-span-1 hidden sm:block border border-t-0  border-black p-4">
             
         </div>
-        <div class="col-span-1 md:col-start-3 border  border-black border-b-0 md:border-b md:border-l-0 p-4 hover:bg-gray-100 transition">
+        <div class="col-span-1 sm:col-start-3 border  border-black border-b-0 sm:border-b sm:border-l-0 p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-red-400"></div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el
             </p>
         </div>
-        <div class="col-span-1 md:col-start-2 lg:col-start-4 border border-black md:border-t-0 md:border-b-0 lg:border-l-0 lg:border-r-0 lg:border-t lg:border-b p-4 hover:bg-gray-100 transition">
+        <div class="col-span-1 sm:col-start-2 lg:col-start-4 border border-black sm:border-t-0 sm:border-b-0 lg:border-l-0 lg:border-r-0 lg:border-t lg:border-b p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-green-400"></div>
@@ -100,14 +100,14 @@
         <div class="col-span-1 border-l border-b hidden lg:block  border-black p-4">
             
         </div>
-        <div class="col-span-1 lg:col-start-2 border  border-black border-b-0  md:border-l-0 border-t-0 lg:border-l p-4 hover:bg-gray-100 transition">
+        <div class="col-span-1 lg:col-start-2 border  border-black border-b-0  sm:border-l-0 border-t-0 lg:border-l p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-purple"></div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el
             </p>
         </div>
-        <div class="col-span-1 lg:col-start-5 border  border-black md:border-r-0 lg:border-t-0 border-b-0  lg:border-r p-4 hover:bg-gray-100 transition">
+        <div class="col-span-1 lg:col-start-5 border  border-black sm:border-r-0 lg:border-t-0 border-b-0  lg:border-r p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-yellow"></div>
@@ -125,7 +125,7 @@
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el
             </p>
         </div>
-        <div class="col-span-1 lg:col-start-5  border-t hidden md:block  border-black p-4">
+        <div class="col-span-1 lg:col-start-5  border-t hidden sm:block  border-black p-4">
             
         </div>
     </div>
