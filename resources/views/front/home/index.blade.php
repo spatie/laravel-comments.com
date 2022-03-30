6e1e9fb56cde0c8636e1cccd3cc0a0d75fa06169
vicdenys <vicdenys@hotmail.com>
2022-03-30 15:56:41 +0200

    wip

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -6,50 +6,50 @@
 
 @section('content')
 
-<section class="grid grid-cols-5 container mx-auto">
-    <div class="col-span-5 inline-grid items-start py-4 grid-cols-5 border-b border-black">
-        <div class=" flex items-center gap-4">
+<section class="grid grid-cols-1 sm:grid-cols-5 container mx-auto px-8 sm:px-0">
+    <div class="col-span-5 inline-grid items-start py-4 grid-cols-1 sm:grid-cols-5  border-b border-black">
+        <div class="col-start-2 md:col-start-1 flex items-center gap-4">
             <a href="https://spatie.be">
-                <img class="h-6" src="/images/logo.svg" alt="logo spatie">
+                <img class="h-6 min-w-12" src="/images/logo.svg" alt="logo spatie">
             </a>
             
             <p class="text-sm">presents</p>
         </div>
-        <h1 class="col-span-3 text-7xl font-normal uppercase leading-[0.74]">/Laravel</h1>
+        <h1 class="col-start-2 mt-4 md:mt-0 col-span-3 text-5xl md:text-7xl font-normal uppercase leading-[0.74]">/Laravel</h1>
     </div>
-    <div class="col-span-5 inline-grid items-start py-4 grid-cols-5 border-b border-black">
-        <h1 class="col-span-3 col-start-2 text-7xl font-bold flex items-end gap-4 uppercase leading-[0.74]">
+    <div class="col-span-5 inline-grid items-start py-4 grid-cols-1 sm:grid-cols-5  border-b border-black">
+        <h1 class="col-span-3 col-start-2 text-5xl md:text-7xl font-bold flex items-end gap-4 uppercase leading-[0.74]">
             Comments
             <a href="https://github.com/spatie/laravel-comments"><img src="/images/github.svg" alt="logo github"></a>
             
         </h1>
 
     </div>
-    <div class="col-span-5 inline-grid items-start py-4 pb-32 grid-cols-5">
+    <div class="col-span-5 inline-grid items-start py-4 pb-16 sm:pb-32 grid-cols-1 sm:grid-cols-5">
             <div class="col-start-2 col-span-2">
                 
                 <p>
-                    <div class="w-3 h-3 inline-block bg-yellow"></div>
-                    A package for laravel. Get a comments section up an running in no time
+                    <div class="w-4 h-4 sm:w-3 sm:h-3 inline-block bg-yellow"></div>
+                    <span class="text-2xl sm:text-base">A package for laravel. Get a comments section up an running in no time</span> 
                 </p>
                 
             </div>
     </div>
     
 </section>
-<section class="grid grid-cols-5 container mx-auto">
-    <div class="col-span-3 col-start-2 grid pb-48 grid-cols-2">
+<section class="grid grid-cols-1 md:grid-cols-5 container px-8 sm:mx-auto">
+    <div class="sm:col-span-3 sm:col-start-2 grid pb-24 sm:pb-48 sm:grid-cols-2 grid-cols-1">
         <div class="pricecard-purple">
             @include('partials.pricecard')
         </div>
         
-        <div class="pricecard-white transform translate-y-12">
+        <div class="pricecard-white transform translate-y-12 -translate-x-px">
             @include('partials.pricecard')
         </div>
     </div>
    
 </section>
-<section class="w-screen ">
+<section class="w-screen px-8 sm:px-0">
     <div class="absolute w-full h-3/4 bg-black top-0 left-0">
 
     </div>
@@ -70,65 +70,68 @@
     </div>
 
 </section>
-<section class="container mx-auto mt-4 pt-20  border-t border-black">
+<section class="container mx-auto mt-4 pt-20  sm:border-t border-black  px-8 sm:px-0">
     <h1 class="uppercase text-7xl mb-8 py-4">Why<span class="font-bold"> This Package</span></h1>
-    <div class="grid grid-cols-5">
-        <div class="col-span-1 border  border-black border-r-0 p-4 hover:bg-gray-100 transition">
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
+        <div class="col-span-1 border  border-black  border-b-0 md:border-b md:border-r-0 p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-yellow"></div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el
             </p>
         </div>
-        <div class="col-span-1 border border-t-0  border-black p-4">
+        <div class="col-span-1 hidden md:block border border-t-0  border-black p-4">
             
         </div>
-        <div class="col-span-1 col-start-3 border  border-black border-l-0 p-4 hover:bg-gray-100 transition">
+        <div class="col-span-1 md:col-start-3 border  border-black border-b-0 md:border-b md:border-l-0 p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-red-400"></div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el
             </p>
         </div>
-        <div class="col-span-1 border border-black border-l-0 border-r-0 p-4 hover:bg-gray-100 transition">
+        <div class="col-span-1 md:col-start-2 lg:col-start-4 border border-black md:border-t-0 md:border-b-0 lg:border-l-0 lg:border-r-0 lg:border-t lg:border-b p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-green-400"></div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el
             </p>
         </div>
-        <div class="col-span-1 border-l border-b  border-black p-4">
+        <div class="col-span-1 border-l border-b hidden lg:block  border-black p-4">
             
         </div>
-        <div class="col-span-1 col-start-2 border  border-black border-b-0 border-t-0 p-4 hover:bg-gray-100 transition">
+        <div class="col-span-1 lg:col-start-2 border  border-black border-b-0  md:border-l-0 border-t-0 lg:border-l p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-purple"></div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el
             </p>
         </div>
-        <div class="col-span-1 col-start-5 border  border-black border-t-0 p-4 hover:bg-gray-100 transition">
+        <div class="col-span-1 lg:col-start-5 border  border-black md:border-r-0 lg:border-t-0 border-b-0  lg:border-r p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-yellow"></div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el
             </p>
         </div>
-        <div class="col-span-1 col-start-2 border-r border-t  border-black p-4">
+        <div class="col-span-1 lg:col-start-2 border-r border-t hidden lg:block  border-black p-4">
             
         </div>
 
-        <div class="col-span-1 col-start-3 border  border-black border-b-0 border-l-0 p-4 hover:bg-gray-100 transition">
+        <div class="col-span-1 lg:col-start-3 border  border-black border-b-0 lg:border-l-0 p-4 hover:bg-gray-100 transition">
             <h2 class="font-bold text-xl">Highly Costumizable</h2>
             <p class="">
                 <div class="w-3 h-3 inline-block bg-red-400"></div>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el
             </p>
         </div>
+        <div class="col-span-1 lg:col-start-5  border-t hidden md:block  border-black p-4">
+            
+        </div>
     </div>
 </section>
-<section class="container mx-auto grid grid-cols-5 border-t border-black">
-    <h1 class="col-span-2 uppercase text-5xl  py-4 border-r border-black">easy<span class="font-bold"> Instalation</span></h1>
+<section class="container mx-auto grid grid-cols-1 md:grid-cols-5 border-t border-black p-8 md:p-0">
+    <h1 class="col-span-2 uppercase text-5xl  py-4 border-b md:border-b-0 md:border-r border-black">easy<span class="font-bold"> Instalation</span></h1>
     <div class="col-span-3 p-4 pr-0">
         <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae ut el</p>
         <div class="w-full relative">
@@ -209,7 +212,7 @@ npm install -what
 </section>
 
 --}}
-<section class="bg-black py-12">
+<section class="bg-black py-12 px-8 md:px-0">
     <div class="container mx-auto text-white">
         <h1 class="text-left uppercase  text-5xl   py-4 ">Other<span class="font-bold"> Products</span></h1>
         <div class="max-w-md">
@@ -219,10 +222,10 @@ npm install -what
             </p>
         </div>
         
-        <div class="grid grid-cols-5 gap-20 mt-12">
-            <div class="relative transform rotate-3 hover:rotate-0 transition group ">
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-20 mt-12">
+            <div class="relative transform rotate-3 hover:rotate-0 transition group px-24">
                 <img src="/images/testing-laravel.jpeg" alt="">
-                <div class="absolute w-full h-full top-0 left-0 p-2 bg-white opacity-0 transition group-hover:opacity-100">
+                <div class="absolute w-full h-full top-0 left-0 p-2 bg-white opacity-0 transition group-hover:opacity-100 ">
                     <a class="w-full h-full cursor-pointer flex items-center justify-center text-black" href="/">
                         <p class="uppercase mt-4 font-bold text-xs">
                             visit site
@@ -231,7 +234,7 @@ npm install -what
                     </a>
                 </div>
             </div>
-            <div class="relative transform -rotate-3 hover:rotate-0 transition group ">
+            <div class="relative transform -rotate-3 hover:rotate-0 transition group px-24 ">
                 <img  src="/images/ray.jpeg" alt="">
                 <div class="absolute w-full h-full top-0 left-0 p-2 bg-white opacity-0 transition group-hover:opacity-100">
                     <a class="w-full h-full cursor-pointer flex items-center justify-center text-black" href="/">
@@ -242,7 +245,7 @@ npm install -what
                     </a>
                 </div>
             </div>
-            <div class="relative transform rotate-3 hover:rotate-0 transition group ">
+            <div class="relative transform rotate-3 hover:rotate-0 transition group px-24 ">
                 <img  src="/images/packagetraining.jpeg" alt="">
                 <div class="absolute w-full h-full top-0 left-0 p-2 bg-white opacity-0 transition group-hover:opacity-100">
                     <a class="w-full h-full cursor-pointer flex items-center justify-center text-black" href="/">
@@ -253,7 +256,7 @@ npm install -what
                     </a>
                 </div>
             </div>
-            <div class="relative transform -rotate-3 hover:rotate-0 transition group ">
+            <div class="relative transform -rotate-3 hover:rotate-0 transition group px-24 ">
                 <img  src="/images/media-library.jpeg" alt="">
                 <div class="absolute w-full h-full top-0 left-0 p-2 bg-white opacity-0 transition group-hover:opacity-100">
                     <a class="w-full h-full cursor-pointer flex items-center justify-center text-black" href="/">
@@ -264,7 +267,7 @@ npm install -what
                     </a>
                 </div>
             </div>
-            <div class="relative transform rotate-3 hover:rotate-0 transition group ">
+            <div class="relative transform rotate-3 hover:rotate-0 transition group px-24 ">
                 <img  src="/images/event-sourcing.jpeg" alt="">
                 <div class="absolute w-full h-full top-0 left-0 p-2 bg-white opacity-0 transition group-hover:opacity-100">
                     <a class="w-full h-full cursor-pointer flex items-center justify-center text-black" href="/">
